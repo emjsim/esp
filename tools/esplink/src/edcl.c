40045a39ce572061e2ecc32f04118f957972476c
biruk-belay <forbiruk@gmail.com>
2025-10-11 18:25:53 -0400

    edcl patch for gcc>=8

--- tools/esplink/src/edcl.c
+++ tools/esplink/src/edcl.c
@@ -153,7 +153,9 @@ static void handle_edcl_message(edcl_snd_t *snd, edcl_rcv_t *rcv)
     int iter       = 0;
     u8 *buf_snd    = malloc(BUFSIZE_MAX_SND * sizeof(u8));
     u8 *buf_rcv    = malloc(BUFSIZE_MAX_RCV * sizeof(u8));
-    socklen_t clen = sizeof(struct sockaddr_in);
+    memset(buf_rcv, 0, BUFSIZE_MAX_RCV * sizeof(u8));
+    memset(buf_snd, 0, BUFSIZE_MAX_SND * sizeof(u8));
+	socklen_t clen = sizeof(struct sockaddr_in);
 
     // Prepare Ethernet packet payload
     set_edcl_msg(buf_snd, snd);
